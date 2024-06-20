03749fcc7539c203b3863a0daff40440b433aabd
刘志健 <liu-zhijian@qq.com>
2024-06-20 20:23:28 +0800

    Update List.js

--- js/List.js
+++ js/List.js
@@ -6,9 +6,11 @@ document.addEventListener('keyup', function (e) {
         jd.style.backgroundColor = '#f9f9f9';
     }
 });
+
 jd.addEventListener('blur', function () {
     jd.style.backgroundColor = '#ebebeb';
 });
+
 jd.addEventListener('focus', function () {
     jd.style.backgroundColor = '#f9f9f9';
 });
@@ -16,6 +18,19 @@ jd.addEventListener('focus', function () {
 $('.sk_con>ul>li').hover(function () {
     $(this).children('ul').stop().slideToggle(400);
 });
+
 $('.sk_list>ul>li').hover(function () {
     $(this).children('ul').stop().slideToggle(400);
 });
+
+/* 使用标签页通信实现购物车里面物品数量的实时显示 */
+/* 使用BroadcastChannel */
+/* 这是接收页 */
+// 创建一个广播通道
+const channel = new BroadcastChannel('myChannel');
+// 监听消息事件
+channel.onmessage = function (event) {
+    const message = event.data;
+    document.querySelector('.count').innerHTML=message;
+    //message是监听到的信息
+};
